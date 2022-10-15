3757f695e00a8fce9eee25ac51a06f4384b30ab4
hytham defrawy <hythamdefrawy@gmail.com>
2022-10-15 11:21:29 -0400

    Adding Validation Exception file

---
+++ CareerCloud.BusinessLogicLayer/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ValidationException : Exception
+    {
+        public object Code { get; set; }
+        public ValidationException(int code, string message) : base(message)
+        {
+            Code = code;
+        }
+    }
+}
+
