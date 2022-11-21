74f68e5a32ad89ea2550719201f9d8f479d104b7
hytham defrawy <hythamdefrawy@gmail.com>
2022-11-21 14:24:46 -0500

    Modified applicant profile method

--- CareerCloud.Pocos/ApplicantSkillPoco.cs
+++ CareerCloud.Pocos/ApplicantSkillPoco.cs
@@ -30,6 +30,8 @@ namespace CareerCloud.Pocos
         public Int32 Importance { get; set; }
         [Column("Time_Stamp")]
         public byte[]? TimeStamp { get; set; } // rowversion
+
+        public virtual ApplicantProfilePoco ApplicantProfile { get; set; }
     }
 }
 
