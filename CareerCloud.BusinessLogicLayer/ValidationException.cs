75dbf50768a49c561ff0b585e5dc2122080e7aa7
hytham defrawy <hythamdefrawy@gmail.com>
2022-11-29 14:53:20 -0500

    Fixed a validation bug

--- CareerCloud.BusinessLogicLayer/ValidationException.cs
+++ CareerCloud.BusinessLogicLayer/ValidationException.cs
@@ -3,11 +3,10 @@ namespace CareerCloud.BusinessLogicLayer
 {
     public class ValidationException : Exception
     {
-        public object Code { get; set; }
+        public int Code { get; set; }
         public ValidationException(int code, string message) : base(message)
         {
             Code = code;
         }
     }
 }
-
