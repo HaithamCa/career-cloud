57c50010b8612873e200e78d295f2fd068853060
hytham defrawy <hythamdefrawy@gmail.com>
2022-11-30 17:45:12 -0500

    Modified applicant job application and security method

--- CareerCloud.Pocos/ApplicantJobApplicationPoco.cs
+++ CareerCloud.Pocos/ApplicantJobApplicationPoco.cs
@@ -22,6 +22,9 @@ namespace CareerCloud.Pocos
 
         [Column("Time_Stamp")]
         public byte[]? TimeStamp { get; set; }
+
+        public virtual ApplicantProfilePoco ApplicantProfile { get; set; }
+        public virtual CompanyJobPoco CompanyJob { get; set; }
     }
 }
 
