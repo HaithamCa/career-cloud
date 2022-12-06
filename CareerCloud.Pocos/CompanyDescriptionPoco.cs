1a5b895e21d7e0c9d8c5913b80be7ae8be5b3849
hytham defrawy <hythamdefrawy@gmail.com>
2022-12-05 21:20:03 -0500

    Solved poco error

--- CareerCloud.Pocos/CompanyDescriptionPoco.cs
+++ CareerCloud.Pocos/CompanyDescriptionPoco.cs
@@ -21,6 +21,9 @@ namespace CareerCloud.Pocos
         public string? CompanyDescription { get; set; }
         [Column("Time_Stamp")]
         public byte[]? TimeStamp { get; set; } // rowversion
+
+        public virtual CompanyProfilePoco CompanyProfile { get; set; }
+        public virtual SystemLanguageCodePoco SystemLanguageCode { get; set; }  
     }
 }
 
