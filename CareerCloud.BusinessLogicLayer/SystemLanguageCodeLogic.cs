b60abf13190d1e69b9bc8b2db28bac76a4129d52
hytham defrawy <hythamdefrawy@gmail.com>
2022-12-09 09:43:18 -0500

    Fixed System language code logic error

--- CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
+++ CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
@@ -3,7 +3,6 @@ using CareerCloud.Pocos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace CareerCloud.BusinessLogicLayer
 {
@@ -15,42 +14,50 @@ namespace CareerCloud.BusinessLogicLayer
             _repository = repository;
         }
 
-        public SystemLanguageCodePoco Get(string languageid)
-        {
-            return _repository.GetSingle(c => c.LanguageID == languageid);
-        }
-
-        public virtual List<SystemLanguageCodePoco> GetAll()
-        {
-            return _repository.GetAll().ToList();
-        }
-
-        protected virtual void Verify(SystemLanguageCodePoco[] pocos)
+        protected void Verify(SystemLanguageCodePoco[] pocos)
         {
             List<ValidationException> exceptions = new List<ValidationException>();
             foreach (SystemLanguageCodePoco poco in pocos)
             {
                 if (string.IsNullOrEmpty(poco.LanguageID))
-                    exceptions.Add(new ValidationException(1000, "Cannot be empty"));
-                if (string.IsNullOrEmpty(poco.Name))
-                    exceptions.Add(new ValidationException(1001, "Cannot be empty"));
-                if (string.IsNullOrEmpty(poco.NativeName))
-                    exceptions.Add(new ValidationException(1002, "Cannot be empty"));
+                {
+                    exceptions.Add(new ValidationException(1000, "Error! Language id cannot be omitted."));
+                }
 
+                if (string.IsNullOrEmpty(poco.Name))
+                {
+                    exceptions.Add(new ValidationException(1001, "Error! Name cannot be omitted."));
+                }
+
+                if (string.IsNullOrEmpty(poco.NativeName))
+                {
+                    exceptions.Add(new ValidationException(1002, "Error! Native name cannot be omitted."));
+                }
             }
 
             if (exceptions.Count > 0)
+            {
                 throw new AggregateException(exceptions);
+            }
         }
 
-        public virtual void Add(SystemLanguageCodePoco[] pocos)
+        public SystemLanguageCodePoco Get(string languageid)
         {
+            return _repository.GetSingle(l => l.LanguageID == languageid);
+        }
 
+        public List<SystemLanguageCodePoco> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        public void Add(SystemLanguageCodePoco[] pocos)
+        {
             Verify(pocos);
             _repository.Add(pocos);
         }
 
-        public virtual void Update(SystemLanguageCodePoco[] pocos)
+        public void Update(SystemLanguageCodePoco[] pocos)
         {
             Verify(pocos);
             _repository.Update(pocos);
