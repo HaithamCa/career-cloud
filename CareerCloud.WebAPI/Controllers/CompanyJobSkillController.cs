8edf5e21b001f0a4b0961486b007f61263e03c5c
hytham defrawy <hythamdefrawy@gmail.com>
2022-12-28 23:21:24 -0500

    Created CompanyJobskillController

---
+++ CareerCloud.WebAPI/Controllers/CompanyJobSkillController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class CompanyJobSkillController : ControllerBase
+    {
+        private CompanyJobSkillLogic _companyJobSkillLogic;
+
+        public CompanyJobSkillController()
+        {
+            var repo = new EFGenericRepository<CompanyJobSkillPoco>();
+            _companyJobSkillLogic = new CompanyJobSkillLogic(repo);
+        }
+        [HttpGet]
+        [Route("jobskill/{CompanyJobSkillId}")]
+        public ActionResult GetCompanyJobSkill(Guid companyJobSkillId)
+        {
+            CompanyJobSkillPoco companyJobSkill = _companyJobSkillLogic.Get(companyJobSkillId);
+            if (companyJobSkill == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(companyJobSkill);
+        }
+
+        [HttpGet]
+        [Route("jobskill")]
+
+        public ActionResult GetCompanyJobSkill()
+        {
+            List<CompanyJobSkillPoco> CompanyJobSkillData = _companyJobSkillLogic.GetAll();
+            return Ok(CompanyJobSkillData);
+        }
+
+        [HttpPost]
+        [Route("jobskill")]
+        public ActionResult PostCompanyJobSkill([FromBody] CompanyJobSkillPoco[] pocos)
+        {
+            _companyJobSkillLogic.Add(pocos);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("jobskill")]
+        public ActionResult PutCompanyJobSkill([FromBody] CompanyJobSkillPoco[] pocos)
+        {
+            _companyJobSkillLogic.Update(pocos);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("jobskill")]
+        public ActionResult DeleteCompanyJobSkill([FromBody] CompanyJobSkillPoco[] pocos)
+        {
+            _companyJobSkillLogic.Delete(pocos);
+            return Ok();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
