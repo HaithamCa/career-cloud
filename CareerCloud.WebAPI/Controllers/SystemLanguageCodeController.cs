7d7f90306ddea50ab3ebcbaeb592df9c41b0914c
hytham defrawy <hythamdefrawy@gmail.com>
2023-01-04 19:28:34 -0500

    Created SystenLanguageCode Controller

---
+++ CareerCloud.WebAPI/Controllers/SystemLanguageCodeController.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class SystemLanguageCodeController : ControllerBase
+    {
+        private SystemLanguageCodeLogic _systemLanguageCodeLogic;
+
+        public SystemLanguageCodeController()
+        {
+            var repo = new EFGenericRepository<SystemLanguageCodePoco>();
+            _systemLanguageCodeLogic = new SystemLanguageCodeLogic(repo);
+        }
+
+        [HttpGet]
+        [Route("education/{SystemLanguageCodeId}")]
+        public ActionResult GetSystemLanguageCode(string systemLanguageCodeId)
+        {
+            SystemLanguageCodePoco systemLanguageCode = _systemLanguageCodeLogic.Get(systemLanguageCodeId);
+            if (systemLanguageCode == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(systemLanguageCode);
+        }
+
+        [HttpGet]
+        [Route("education")]
+
+        public ActionResult GetSystemLanguageCode()
+        {
+            List<SystemLanguageCodePoco> systemLanguageCodeData = _systemLanguageCodeLogic.GetAll();
+            return Ok(systemLanguageCodeData);
+        }
+
+        [HttpPost]
+        [Route("education")]
+        public ActionResult PostSystemLanguageCode([FromBody] SystemLanguageCodePoco[] pocos)
+        {
+            _systemLanguageCodeLogic.Add(pocos);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("education")]
+        public ActionResult PutSystemLanguageCode([FromBody] SystemLanguageCodePoco[] pocos)
+        {
+            _systemLanguageCodeLogic.Update(pocos);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("education")]
+        public ActionResult DeleteSystemLanguageCode([FromBody] SystemLanguageCodePoco[] pocos)
+        {
+            _systemLanguageCodeLogic.Delete(pocos);
+            return Ok();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
