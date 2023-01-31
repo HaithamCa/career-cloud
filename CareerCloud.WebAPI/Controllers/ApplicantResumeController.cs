d39b6ddf97cf5848604c1ccdc776b6b4d7d0632a
hytham defrawy <hythamdefrawy@gmail.com>
2023-01-31 16:52:55 -0500

    Modified ApplicantResumeController.cs

---
+++ CareerCloud.WebAPI/Controllers/ApplicantResumeController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class ApplicantResumeController : ControllerBase
+    {
+        private ApplicantResumeLogic _applicantResumeLogic;
+
+        public ApplicantResumeController()
+        {
+            var repo = new EFGenericRepository<ApplicantResumePoco>();
+            _applicantResumeLogic = new ApplicantResumeLogic(repo);
+        }
+        [HttpGet]
+        [Route("resume/{ApplicantResumeId}")]
+        public ActionResult GetApplicantResume(Guid applicantResumeId)
+        {
+            ApplicantResumePoco applicantResume = _applicantResumeLogic.Get(applicantResumeId);
+            if (applicantResume == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(applicantResume);
+        }
+
+        [HttpGet]
+        [Route("resume")]
+
+        public ActionResult GetApplicantResume()
+        {
+            List<ApplicantResumePoco> applicantResumeData = _applicantResumeLogic.GetAll();
+            return Ok(applicantResumeData);
+        }
+
+        [HttpPost]
+        [Route("resume")]
+        public ActionResult PostApplicantResume([FromBody] ApplicantResumePoco[] pocos)
+        {
+            _applicantResumeLogic.Add(pocos);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("resume")]
+        public ActionResult PutApplicantResume([FromBody] ApplicantResumePoco[] pocos)
+        {
+            _applicantResumeLogic.Update(pocos);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("resume")]
+        public ActionResult DeleteApplicantResume([FromBody] ApplicantResumePoco[] pocos)
+        {
+            _applicantResumeLogic.Delete(pocos);
+            return Ok();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
