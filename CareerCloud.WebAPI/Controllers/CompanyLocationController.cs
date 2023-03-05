93bfb73ebdcffc6e4ff2bb3d7b86fed4f4f1412a
hytham defrawy <hythamdefrawy@gmail.com>
2023-03-04 21:55:47 -0500

    Created CompanyLocation Controller

---
+++ CareerCloud.WebAPI/Controllers/CompanyLocationController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class CompanyLocationController : ControllerBase
+    {
+        private CompanyLocationLogic _companyLocationLogic;
+
+        public CompanyLocationController()
+        {
+            var repo = new EFGenericRepository<CompanyLocationPoco>();
+            _companyLocationLogic = new CompanyLocationLogic(repo);
+        }
+
+        [HttpGet]
+        [Route("location/{CompanyLocationId}")]
+        public ActionResult GetCompanyLocation(Guid companyLocationId)
+        {
+            CompanyLocationPoco companyLocation = _companyLocationLogic.Get(companyLocationId);
+            if (companyLocation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(companyLocation);
+        }
+
+        [HttpGet]
+        [Route("location")]
+
+        public ActionResult GetCompanyLocation()
+        {
+            List<CompanyLocationPoco> companyLocationData = _companyLocationLogic.GetAll();
+            return Ok(companyLocationData);
+        }
+
+        [HttpPost]
+        [Route("location")]
+        public ActionResult PostCompanyLocation([FromBody] CompanyLocationPoco[] pocos)
+        {
+            _companyLocationLogic.Add(pocos);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("location")]
+        public ActionResult PutCompanyLocation([FromBody] CompanyLocationPoco[] pocos)
+        {
+            _companyLocationLogic.Update(pocos);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("location")]
+        public ActionResult DeleteCompanyLocation([FromBody] CompanyLocationPoco[] pocos)
+        {
+            _companyLocationLogic.Delete(pocos);
+            return Ok();
+        }
+    }
+}
